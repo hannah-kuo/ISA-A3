233dfac0b8abf5282da50928c811944fd7cab7f6
hannah-kuo <hannahkuo0205@gmail.com>
2023-04-09 15:01:18 -0700

    Adjusted styling of pokestats card

--- client/src/components/pokemonComponent.js
+++ client/src/components/pokemonComponent.js
@@ -1,5 +1,5 @@
 import Pokemon from "./pokemon";
-import { Col, Container, Row, Card, Button} from "react-bootstrap";
+import { Col, Container, Row, Card, Button } from "react-bootstrap";
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { apiGetSinglePokemon } from "../api/pokeAPI";
@@ -8,90 +8,101 @@ import '../styles/app.css';
 
 function PokemonPage({ currentPokemon, currentPage }) {
 
-    const [ individualPoke, setIndividualPoke ] = useState("");
+    const [individualPoke, setIndividualPoke] = useState("");
 
     const getMoreInfo = async (pokeID) => {
         apiGetSinglePokemon(pokeID)
-        .then(res => res.data)
-        .then(res => {
-          setIndividualPoke(res);
-        })
-        .catch(err => console.log(err));
+            .then(res => res.data)
+            .then(res => {
+                setIndividualPoke(res);
+            })
+            .catch(err => console.log(err));
         renderMoreInfo(individualPoke[0]);
     };
 
     const formatExtraInformation = (singlePoke) => {
-        return(
-            <>
-            <Card.Text>Type: {singlePoke.type.map(item => ' ' + item)} </Card.Text>
-            <Card.Text>Base Stats: </Card.Text>
-            <Card.Text>HP: {singlePoke.base.HP} </Card.Text>
-            <Card.Text>Attack: {singlePoke.base.Attack} </Card.Text>
-            <Card.Text>Defence: {singlePoke.base.Defense} </Card.Text>
-            </>
+        return (
+            <div className="extra-info">
+                <Card.Text className="type">Type: {singlePoke.type.map((item) => ' ' + item)}</Card.Text>
+                <Card.Text className="base-stats">Base Stats:</Card.Text>
+                <div className="stats">
+                    <div className="stat">
+                        <Card.Text>HP</Card.Text>
+                        <Card.Text>{singlePoke.base.HP}</Card.Text>
+                    </div>
+                    <div className="stat">
+                        <Card.Text>Attack</Card.Text>
+                        <Card.Text>{singlePoke.base.Attack}</Card.Text>
+                    </div>
+                    <div className="stat">
+                        <Card.Text>Defence</Card.Text>
+                        <Card.Text>{singlePoke.base.Defense}</Card.Text>
+                    </div>
+                </div>
+            </div>
         );
     };
 
     const renderMoreInfo = (singlePoke) => {
-        return(
-            <Card.Body> 
-                <Card.Title style={ { textAlign: 'center' } }> { singlePoke.name.english } </Card.Title>
+        return (
+            <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}> {singlePoke.name.english} </Card.Title>
                 {formatExtraInformation(singlePoke)}
-                <div style={ {textAlign: 'center'}}> 
-                                <Button 
-                                variant="dark"
-                                size="sm"
-                                className="learn-more-button"
-                                onClick={() => setIndividualPoke("")}
-                                > 
-                                Minimize 
-                                </Button>
-                            </div>
+                <div style={{ textAlign: 'center' }}>
+                    <Button
+                        variant="dark"
+                        size="sm"
+                        className="learn-more-button"
+                        onClick={() => setIndividualPoke("")}
+                    >
+                        Minimize
+                    </Button>
+                </div>
             </Card.Body>
         );
     };
 
-    const renderBasicCard = ( item ) => {
+    const renderBasicCard = (item) => {
 
-        return(
-            <Card.Body> 
-                <Card.Title style={ { textAlign: 'center' } }> { item.name.english } </Card.Title>
-                <Pokemon key = { item.id } pokemon= { item } />
-                <div style={ {textAlign: 'center'}}> 
-                    <Button 
-                    variant="dark"
-                    size="sm"
-                    className="learn-more-button"
-                    onClick={() => getMoreInfo(item.id)}
-                    > 
-                    Learn More 
+        return (
+            <Card.Body>
+                <Card.Title style={{ textAlign: 'center' }}> {item.name.english} </Card.Title>
+                <Pokemon key={item.id} pokemon={item} />
+                <div style={{ textAlign: 'center' }}>
+                    <Button
+                        variant="dark"
+                        size="sm"
+                        className="learn-more-button"
+                        onClick={() => getMoreInfo(item.id)}
+                    >
+                        Learn More
                     </Button>
                 </div>
             </Card.Body>
         )
     };
 
-    return(
-        <div> 
+    return (
+        <div>
 
-        <h1 style={ {textAlign: 'center' }}>
-            Page {currentPage}
-        </h1> 
+            <h1 style={{ textAlign: 'center' }}>
+                Page {currentPage}
+            </h1>
 
-        <Container>        
-        <Row xs={2} md={5}>
-        {currentPokemon.map(item => (
-                <Col>
-                    <Card>
-                        { individualPoke && individualPoke[0].id === item.id ? renderMoreInfo(item) : renderBasicCard(item)}
+            <Container>
+                <Row xs={2} md={5}>
+                    {currentPokemon.map(item => (
+                        <Col>
+                            <Card>
+                                {individualPoke && individualPoke[0].id === item.id ? renderMoreInfo(item) : renderBasicCard(item)}
 
-                    </Card>
-                </Col>
-            ))}
-        </Row>
-    </Container>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
 
-    </div>
+        </div>
     );
 };
 
